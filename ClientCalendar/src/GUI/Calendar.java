203d3b17b7b3d757d7d5cd4114d4de240b06d5ff
kare12ac <katrine.refvem@gmail.com>
2014-11-28 12:10:37 +0100

    Login

---
+++ ClientCalendar/src/GUI/Calendar.java
@@ -0,0 +1,35 @@
+package GUI;
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class Calendar extends JFrame{
+	public static void main (String []args){
+		Calendar frameTabel = new Calendar();
+	}
+		JLabel welcome = new JLabel("Welcome to CBS Calendar");
+		JPanel panel = new JPanel();
+		
+		Calendar(){
+			super("CBS Calendar");
+			setSize(1000,1000);
+			setLocation(500,280);
+			getContentPane().setLayout(null);
+			panel.setBounds(0, 0, 1000, 978);
+			panel.setLayout(null);
+			welcome.setForeground(SystemColor.textHighlight);
+			welcome.setFont(new Font("Apple Braille", Font.PLAIN, 28));
+			
+			welcome.setBounds(344,6,392,160);
+			panel.add(welcome);
+			
+			getContentPane().add(panel);
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			setVisible(true);
+			
+		}
+	
+	
+}
